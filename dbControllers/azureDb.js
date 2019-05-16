8e5aa2fefb9682cf12c48125e760965e908a4720
yishaia zabary <yz@sky-pick.com>
2019-05-16 20:31:09 +0300

    now the azureDb.js file exports a method called queryDatabase. that function receives an sql query as a string and shoots a request to our db server. use this in the app.js like so: 1. in app.js add the line "const azureControler = require("dbControllers/azureDb");" (already done by me). 2. anywhere you want to use the function just write "azureControler.queryDatabase(THE_SQL_QUERY);".

--- dbControllers/azureDb.js
+++ dbControllers/azureDb.js
@@ -1,54 +1,52 @@
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
 
-// Create connection to database
-var config =
-    {
-        authentication: {
-            options: {
-                userName: 'tamir', // update me
-                password: 'Db123456' // update me
+export function queryDatabase(sqlQuery)
+{
+    // Create connection to database
+    var config =
+        {
+            authentication: {
+                options: {
+                    userName: 'tamir', // update me
+                    password: 'Db123456' // update me
+                },
+                type: 'default'
             },
-            type: 'default'
-        },
-        server: 'appwebserver2.database.windows.net', // update me
-        options:
-            {
-                database: 'Assignment3db', //update me
-                encrypt: true
-            }
-    };
-var connection = new Connection(config);
+            server: 'appwebserver2.database.windows.net', // update me
+            options:
+                {
+                    database: 'Assignment3db', //update me
+                    encrypt: true
+                }
+        };
+    var connection = new Connection(config);
 
 // Attempt to connect and execute queries if connection goes through
-connection.on('connect', function(err)
-    {
-        if (err)
+    connection.on('connect', function(err)
         {
-            console.log(err)
+            if (err)
+            {
+                console.log(err)
+            }
+            else
+            {
+                queryDatabase()
+            }
         }
-        else
-        {
-            queryDatabase()
-        }
-    }
-);
-
-function queryDatabase()
-{
-    console.log('Reading rows from the Table...');
+    );
+    console.log('your query: '+ sqlQuery);
 
     // Read all rows from table
     var request = new Request(
-        "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName FROM [SalesLT].[ProductCategory] pc "
-        + "JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid",
+        sqlQuery,
         function(err, rowCount, rows)
         {
             console.log(rowCount + ' row(s) returned');
             process.exit();
         }
     );
-
+//prints out the actual data
     request.on('row', function(columns) {
         columns.forEach(function(column) {
             console.log("%s\t%s", column.metadata.colName, column.value);
