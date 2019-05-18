10f1ba85a01e2de69b5d6c1d025990b6764c9f18
yishaia zabary <yz@sky-pick.com>
2019-05-18 17:46:11 +0300

    fixed azureDb.js

--- dbControllers/azureDb.js
+++ dbControllers/azureDb.js
@@ -1,6 +1,25 @@
 
 module.exports.queryDatabase= function (sqlQuery)
 {
+    function queryDataBase(query){
+        console.log('Reading rows from the Table...');
+
+        // Read all rows from table
+        var request = new Request(query,
+            function(err, rowCount, rows)
+            {
+                console.log(rowCount + ' row(s) returned');
+                process.exit();
+            }
+        );
+
+        request.on('row', function(columns) {
+            columns.forEach(function(column) {
+                console.log("%s\t%s", column.metadata.colName, column.value);
+            });
+        });
+        connection.execSql(request);
+    }
     var Connection = require('tedious').Connection;
     var Request = require('tedious').Request;
     // Create connection to database
@@ -16,41 +35,24 @@ module.exports.queryDatabase= function (sqlQuery)
             server: 'appwebserver2.database.windows.net', // update me
             options:
                 {
-                    database: 'Assignment3db', //update me
+                    database: 'TutorialDb', //update me
                     encrypt: true
                 }
         };
     var connection = new Connection(config);
-
+    // console.log(connection);
 // Attempt to connect and execute queries if connection goes through
     connection.on('connect', function(err)
         {
+            console.log("hello");
             if (err)
             {
-                console.log(err)
+                console.log(err+"hey")
             }
             else
             {
-                queryDatabase()
+                queryDataBase(sqlQuery);
             }
         }
     );
-    console.log('your query: '+ sqlQuery);
-
-    // Read all rows from table
-    var request = new Request(
-        sqlQuery,
-        function(err, rowCount, rows)
-        {
-            console.log(rowCount + ' row(s) returned');
-            process.exit();
-        }
-    );
-//prints out the actual data
-    request.on('row', function(columns) {
-        columns.forEach(function(column) {
-            console.log("%s\t%s", column.metadata.colName, column.value);
-        });
-    });
-    connection.execSql(request);
 };
